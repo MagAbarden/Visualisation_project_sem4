30c95064cf18979dc57b2c2d46e99db23f34f5a7
MagAbarden <161757742+MagAbarden@users.noreply.github.com>
2025-05-18 16:38:22 +0200

    drukarka 3d inny model
    Jeszcze nie ma głowicy

--- mait_test.py
+++ mait_test.py
@@ -1,23 +1,49 @@
-import vpython as vp
-from time import *
-plate_thickness = .1 
-plate_sideX = 10
-plate_sideY = 2
-plate_sideZ = 10
-pingRadius = .5
-base = vp.box(pos = vp.vector(0,-plate_sideY/2,0), color = vp.color.white, length = plate_sideX, height = plate_thickness, width = plate_sideZ)
-#roof = vp.box(pos = vp.vector(0,plate_sideY/2,0), color = vp.color.white, length = plate_sideX, height = plate_thickness, width = plate_sideZ)
-wall_right = vp.box(pos = vp.vector(plate_sideX/2,0,0), color = vp.color.white, length = plate_thickness, height = plate_sideY, width = plate_sideZ)
-wall_left = vp.box(pos = vp.vector(-plate_sideX/2,0,0), color = vp.color.white, length = plate_thickness, height = plate_sideY, width = plate_sideZ)
-wall_back = vp.box(pos = vp.vector(0,0,-plate_sideZ/2), color = vp.color.white, length = plate_sideX, height = plate_sideY, width = plate_thickness)
-wall_front = vp.box(pos = vp.vector(0,0,plate_sideZ/2), color = vp.color.white, length = plate_sideX, height = plate_sideY, width = plate_thickness)
-ping_pong_ball = vp.sphere(radius = pingRadius, color = vp.color.red)
-delta_ping = .01
-ping = 0
+from vpython import *
+import numpy as np
+canvas(width=1280, height=720, background=color.white)
+#PARAMETERS FOR 3D PRINTER DETAILS
+
+#3D printer plate parameters
+plate_thickness  = 0.2
+plate_width = 10
+plate_lenght = 10
+#3D printers vertical support beams parameters
+vert_supp_thickness = 0.2
+vert_supp_height = 10
+#3D printers horizontal support beams parameters
+hor_supp_thickness = 0.2
+hor_supp_lenght = 10.4
+#3D printer base plate 
+printer_plate = box(pos=vector(0,0,0), size = vector(plate_lenght,plate_thickness,plate_width), texture=textures.rock)
+#3D printer vertical beams  
+printer_vert_supp_beamR1 = box(pos=vector(5.1,4.9,.2), size = vector(vert_supp_thickness,vert_supp_height,vert_supp_thickness), color = color.red)
+printer_vert_supp_beamR2 = box(pos=vector(5.1,4.9,-.2), size = vector(vert_supp_thickness,vert_supp_height,vert_supp_thickness), color = color.red)
+printer_vert_supp_beamL1 = box(pos=vector(-5.1,4.9,.2), size = vector(vert_supp_thickness,vert_supp_height,vert_supp_thickness), color = color.red)
+printer_vert_supp_beamL2 = box(pos=vector(-5.1,4.9,-.2), size = vector(vert_supp_thickness,vert_supp_height,vert_supp_thickness), color = color.red)
+#3D printer horizontal beams 
+printer_hor_supp_beam1 = box(pos=vector(0,.2,0), size = vector(hor_supp_lenght,hor_supp_thickness,hor_supp_thickness), color = color.blue)
 while True:
-    vp.rate(120)
-    ping = ping+ delta_ping
-    if(ping > (plate_sideX/2)-pingRadius-plate_thickness or ping <-(plate_sideX/2)+pingRadius+plate_thickness):
-        delta_ping = delta_ping*(-1)
-    ping_pong_ball.pos = vp.vector(ping,0,0)
-    pass
+        rate(30)
+        key = keysdown()
+        if 'w' in key:
+                if printer_hor_supp_beam1.pos.y <= 9.7:
+                        printer_hor_supp_beam1.pos.y += 0.1       
+        elif 's' in key:
+                if printer_hor_supp_beam1.pos.y >= 0.2:
+                        printer_hor_supp_beam1.pos.y += -0.1
+        if 'up' in key:
+                if printer_vert_supp_beamR1.pos.z <= 4.8:
+                        printer_vert_supp_beamR1.pos.z += 0.1
+                        printer_vert_supp_beamR2.pos.z += 0.1
+                        printer_vert_supp_beamL1.pos.z += 0.1
+                        printer_vert_supp_beamL2.pos.z += 0.1
+                        printer_hor_supp_beam1.pos.z += 0.1
+        elif 'down' in key:
+                if printer_vert_supp_beamR2.pos.z >= -4.8:
+                        printer_vert_supp_beamR1.pos.z -= 0.1
+                        printer_vert_supp_beamR2.pos.z -= 0.1
+                        printer_vert_supp_beamL1.pos.z -= 0.1
+                        printer_vert_supp_beamL2.pos.z -= 0.1
+                        printer_hor_supp_beam1.pos.z -= 0.1
+        elif 'q' in key:
+                break
