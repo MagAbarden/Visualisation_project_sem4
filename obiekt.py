96c368d33500fff1f1a23cfd05d78bdcf9ee2565
Quarex226 <ezegarowicz@gmail.com>
2025-05-24 15:16:41 +0200

    Update obiekt.py
    Added printable objects

--- obiekt.py
+++ obiekt.py
@@ -4,11 +4,14 @@ import numpy as np
 canprint = False #Check if you can start printing figures
 ifcenter = False #Check for alignment of the printer head
 figurechoice = 0 #Check which figure
+
 i = 0
+j = 0
+go_value = 0.1
 
 #Simulation captions
 scene.caption = """Right button drag or Ctrl-drag to rotate "camera" to view scene.
-To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
+To zoom, drag with middle button or Alt/Opti`on depressed, or use scroll wheel.
     On a two-button mouse, middle is left + right.
 Shift-drag to pan left/right and up/down.
 
@@ -74,18 +77,18 @@ def whichfigure(evt):
         global figurechoice
         if evt.text == 'Rectangle':
                 figurechoice = 0
-        if evt.text == 'Spring':
-                figurechoice = 0
-        if evt.text == 'Ball':
-                figurechoice = 0
+        elif evt.text == 'Spring':
+                figurechoice = 1
+        elif evt.text == 'Ball  ':
+                figurechoice = 2
 
 def buildorder():
         global ifcenter
         ifcenter = True
         
 rectangle = radio(bind=whichfigure, text='Rectangle', name='figures', checked=True)
-spring = radio(bind=whichfigure, text='Spring', name='figures', checked=True)
-ball = radio(bind=whichfigure, text='Ball', name='figures', checked=True)
+spring = radio(bind=whichfigure, text='Spring', name='figures')
+ball = radio(bind=whichfigure, text='Ball  ', name='figures')
 
 build = button (bind = buildorder, text = "build")
 
@@ -153,17 +156,117 @@ while True:
         #Start printing
         if canprint == True:
                 if figurechoice == 0: #Rectangle
-                        support_x.pos.x += 0.05
-                        crane.pos.x += 0.05
-                        head.pos.x += 0.05
-                        i += 1
-                        if i == 10:
-                                canprint = False
-                                head.make_trail = False
-                                i = 0
-                                
-                
-                        
-                        
-                
+                        if j < 16 :
+                                if i < 16:
+                                        support_x.pos.x += go_value
+                                        crane.pos.x += go_value
+                                        head.pos.x += go_value
+                                        i += 1
+                                if i == 16:
+                                        support_z.pos.z += 0.1
+                                        crane.pos.z += 0.1
+                                        head.pos.z += 0.1
+                                        i = 0
+                                        go_value = -go_value
+                                        j += 1
+                        if 15 < j < 24:
+                                if i < 16:
+                                        support_x.pos.x += 0.1
+                                        crane.pos.x += 0.1
+                                        head.pos.x += 0.1
+                                elif 15 < i < 32:
+                                        support_z.pos.z += -0.1
+                                        crane.pos.z += -0.1
+                                        head.pos.z += -0.1
+                                elif 31 < i < 48:
+                                        support_x.pos.x -= 0.1
+                                        crane.pos.x -= 0.1
+                                        head.pos.x -= 0.1
+                                elif 47 < i < 64:
+                                        support_z.pos.z -= -0.1
+                                        crane.pos.z -= -0.1
+                                        head.pos.z -= -0.1        
+                                i += 1
+                                if i == 64:
+                                        crane.pos.y += 0.2
+                                        head.pos.y += 0.2
+                                        i = 0
+                                        j += 1
+                        if 23 < j < 40:
+                                if i < 16:
+                                        support_x.pos.x += go_value
+                                        crane.pos.x += go_value
+                                        head.pos.x += go_value
+                                        i += 1
+                                if i == 16:
+                                        support_z.pos.z += -0.1
+                                        crane.pos.z += -0.1
+                                        head.pos.z += -0.1
+                                        i = 0
+                                        go_value = -go_value
+                                        j += 1
+                        if j == 40:
+                               head.make_trail = False
+                               canprint = False
+                               i = 0
+                               j = 0
         
+                if figurechoice == 1: #Spring
+                        if j == 0:
+                                support_z.pos.z += 0.05
+                                crane.pos.z += 0.05
+                                head.pos.z += 0.05
+                                i += 1
+                                if i == 19:
+                                        i = 0
+                                        j = 1
+                        if 0 < j < 8:
+                                if i < 2*np.pi:
+                                        support_x.pos.x = np.sin(i)
+                                        crane.pos.x = np.sin(i)
+                                        head.pos.x = np.sin(i)
+                                        
+                                        support_z.pos.z = np.cos(i)
+                                        crane.pos.z = np.cos(i)
+                                        head.pos.z = np.cos(i)
+                                        
+                                        crane.pos.y += 0.01
+                                        head.pos.y += 0.01
+                                        
+                                        i = i + 0.05*np.pi
+                                if i > 2*np.pi-0.01:
+                                        i = 0
+                                        j += 1
+                                        
+                        if j == 8:
+                                j = 0
+                                i = 0
+                                head.make_trail = False
+                                canprint = False 
+                                there_is_object = True
+                
+                if figurechoice == 2: #Ball
+                        if j < 2:
+                                j_fun = np.sqrt(1-(j-1)**2)
+                                if i < 2*np.pi:
+                                        support_x.pos.x = np.sin(i) * j_fun
+                                        crane.pos.x = np.sin(i) * j_fun
+                                        head.pos.x = np.sin(i) * j_fun
+                                        
+                                        support_z.pos.z = np.cos(i) * j_fun
+                                        crane.pos.z = np.cos(i) * j_fun
+                                        head.pos.z = np.cos(i) * j_fun
+                                        i = i + 0.05*np.pi
+                                if i > 2*np.pi-0.01:
+                                        i = 0
+                                        crane.pos.y += 0.1
+                                        head.pos.y += 0.1
+                                        j += 0.1
+                                        
+                                        print(j)
+                        if j > 1.905:
+                                j = 0
+                                i = 0
+                                head.make_trail = False
+                                canprint = False 
+                                there_is_object = True  
